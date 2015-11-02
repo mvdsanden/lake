e674a12ffc51f9f3407b75fc007481c3245883b4
Marten van de Sanden <mart.vd.sanden@gmail.com>
2015-11-02 16:09:43 -0500

    Initial Commit

---
+++ Lake/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lake
+//
+//  Created by Marten van de Sanden on 11/2/15.
+//  Copyright © 2015 MVDS. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
